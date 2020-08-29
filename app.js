414079e61e52a77893653e5684782360bd8a3657
Ben G <bensgar92@gmail.com>
2020-08-29 15:37:03 -0600

    updated app.js with finished inquirer prompts

--- app.js
+++ app.js
@@ -11,39 +11,117 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-var employeeArray=[];
+const employeeArray=[];
 
-const promptUser = () => {
+
+function init() {
+    inquirer.prompt([//initial question to be asked which will run an if function to evaluate the choice and run additional questions
+        {
+            type: "list",
+            message: "What type of employee would you like to input?",
+            name: "name",
+            choices: ["Manager", "Engineer", "Intern", "No more employees left!"]
+        },
+    ]).then(choice => {
+        if (choice.name === "Manager") {
+            newManager();
+        } else if (choice.name === "Engineer") {
+            newEngineer();
+        } else if (choice.name === "Intern") {
+            newIntern();
+        } else generateHTML(outputPath, render(employeeArray));
+    });
+};
+
+function newManager() {
     return inquirer.prompt([
         {
             type: "input",
-            name: "name",
-            message: "What is your name?"
+            message: "What is the manager's name?",
+            name: "name"
         },
         {
             type: "input",
-            name: "id",
-            message: "What is your id number?"
+            message: "What is the manager's ID?",
+            name: "id"
         },
         {
             type: "input",
-            name: "email",
-            message: "What is your email?"
+            message: "What is the manager's email?",
+            name: "email"
         },
         {
-            type: "list",
-            name: "position",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern"
-            ],
-            message: "What is your position?"
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "officeNumber"
         }
-    ])//use .then to do something
+    ]).then(function(answer) {
+        let manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber)
+        employeeArray.push(manager);
+
+        init();
+    })
+};
+
+function newEngineer(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the engineers's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's github username?",
+            name: "github"
+        }
+    ]).then(function(answer) {
+        let engineer = new Engineer(answer.name, answer.id, answer.email, answer.github)
+        employeeArray.push(engineer);
+
+        init();
+    })
 }
 
-function newEngineer(answer){
+function newIntern() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the intern's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the intern's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the intern's school?",
+            name: "school"
+        }
+    ]).then(function(answer) {
+        let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+        employeeArray.push(intern);
+
+        init();
+    })
+}
     //answers container name, id, and email
     //ask for github and if they are done
     // var newGuy = new Engineer(answer.name,ans,id, ans.email, response.github)
@@ -54,7 +132,25 @@ function newEngineer(answer){
 
         //if you are done, you call a fx to generate the cards passing in your array of obj
         //if not done go back and ask the questions again
-}
+
+
+
+
+
+function generateHTML(fileName, data) {
+    fs.writeFile(fileName, data, "utf8", function(err) {
+        if (err) {
+            throw err;
+        };
+        console.log("HTML page has successfully rendered!");
+    });
+};
+
+init();//init function calls the first questions to determine which employee to add
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -77,3 +173,9 @@ function newEngineer(answer){
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+//MY PSEUDO CODE - TO DO:
+//I want to ask the user a question for who they want to make an employee card for
+//Based on the selection I want to fire the right function which then asks the user questions about that employee(name, id, email, office/github/school)
+//at the end of the questions the user will be asked to add more employees or be finished
+//if finished, the answers will be saved in an array? and then pushed into the htmlrenderer?
