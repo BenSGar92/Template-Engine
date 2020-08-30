da83bad8e62b358927bef295dc1e7ff04b0e9e98
Ben G <bensgar92@gmail.com>
2020-08-30 14:10:12 -0600

    added screenshots

--- app.js
+++ app.js
@@ -133,35 +133,3 @@ function generateHTML(fileName, data) {//functions that generates the html if do
 };
 
 init();//init function calls the first questions to determine which employee to add
-
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-//MY PSEUDO CODE - TO DO:
-//I want to ask the user a question for who they want to make an employee card for
-//Based on the selection I want to fire the right function which then asks the user questions about that employee(name, id, email, office/github/school)
-//at the end of the questions the user will be asked to add more employees or be finished
-//if finished, the answers will be saved in an array? and then pushed into the htmlrenderer?
